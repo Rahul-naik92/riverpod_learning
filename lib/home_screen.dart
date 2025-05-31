e414cbcb3ed7e1662591ecad8ebd8292dbd58b2b
Rahul-naik92 <team.rahulnaik@gmail.com>
2025-05-31 14:29:15 +0530

    Updated Code with State

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,40 +1,76 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-
-final hello = Provider<String>((ref){
-
-  return "Subscribe";
-
+import 'package:riverpod_learning/slider_provider.dart';
+final hello = Provider<String>((ref) {
+  return "Subscribe My Channel";
 });
 
-class HomeScreen extends ConsumerStatefulWidget {
-  const HomeScreen({super.key});
+final counter = StateProvider<int>((ref) {
+  return 0;
+});
 
-  @override
-  ConsumerState<HomeScreen> createState() => _HomeScreenState();
-}
+final switchProvider = StateProvider<bool>((ref) {
+  return false;
+});
 
-class _HomeScreenState extends ConsumerState<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    final subscribe=ref.watch(hello);
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text(subscribe),),
-    );
-  }
-}
 
-// class HomeScreen extends ConsumerWidget {
+
+// class HomeScreen extends ConsumerStatefulWidget {
 //   const HomeScreen({super.key});
-
+//
 //   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final subScribe = ref.watch(hello);
-//     return Scaffold(
-//       appBar: AppBar(),
-//       body: Center(child: Text(subScribe),),
+//   ConsumerState<HomeScreen> createState() => _HomeScreenState();
+// }
+//
+// class _HomeScreenState extends ConsumerState<HomeScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final myHello = ref.watch(hello);
+//         return Scaffold(
+//       appBar: AppBar(title: Text("Learning Riverpod"), centerTitle: true,),
+//       body: Center(
+//         child: Text("Hello $myHello"),
+//       ),
 //     );
 //   }
 // }
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    print('Build');
+    return Scaffold(
+      appBar: AppBar(title: Text("Learning Riverpod"), centerTitle: true),
+      body: Column(
+        children: [
+          Consumer(builder: (context, ref, child){
+            final simpleCount = ref.watch(counter);
+            print('Build--2');
+            return  Center(
+              child: Text(simpleCount.toString(), style: TextStyle(fontSize: 20)),
+            );
+          }),
+
+          Consumer(builder: (context, ref, child){
+            final switchProviderex = ref.watch(switchProvider);
+            print('Build--3');
+            return Switch(value: switchProviderex, onChanged: (value){
+              //ref.read(switchProviderex.notifier).re
+              ref.read(switchProvider.notifier).state=value;
+            });
+          }),
+
+          
+          ElevatedButton(onPressed: (){
+            ref.read(counter.notifier).state++;
+          }, child: Text('+'))
+        ],
+      ),
+    );
+  }
+}
