d02b92c825e8a2d992b6eedebf008e61af69542b
Rahul-naik92 <team.rahulnaik@gmail.com>
2025-05-31 17:22:21 +0530

    Handling multiple states.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,72 +3,59 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_learning/slider_provider.dart';
+
 final hello = Provider<String>((ref) {
   return "Subscribe My Channel";
 });
-
-final counter = StateProvider<int>((ref) {
-  return 0;
-});
-
-final switchProvider = StateProvider<bool>((ref) {
-  return false;
-});
-
-
-
-// class HomeScreen extends ConsumerStatefulWidget {
-//   const HomeScreen({super.key});
-//
-//   @override
-//   ConsumerState<HomeScreen> createState() => _HomeScreenState();
-// }
-//
-// class _HomeScreenState extends ConsumerState<HomeScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     final myHello = ref.watch(hello);
-//         return Scaffold(
-//       appBar: AppBar(title: Text("Learning Riverpod"), centerTitle: true,),
-//       body: Center(
-//         child: Text("Hello $myHello"),
-//       ),
-//     );
-//   }
-// }
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print("Widget Build");
 
-    print('Build');
     return Scaffold(
-      appBar: AppBar(title: Text("Learning Riverpod"), centerTitle: true),
+      appBar: AppBar(title: Text("Counter Application")),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Consumer(builder: (context, ref, child){
-            final simpleCount = ref.watch(counter);
-            print('Build--2');
-            return  Center(
-              child: Text(simpleCount.toString(), style: TextStyle(fontSize: 20)),
+            final slider = ref.watch(sliderProvider.select((state)=> state.showPassword));
+            print("Build 1");
+            return GestureDetector(onTap: (){
+              final stateProvider = ref.read(sliderProvider.notifier);
+              stateProvider.state = stateProvider.state.copyWith(showPassword: !slider);
+            },child: Container(
+              height: 200,
+              width: 200,
+              child: slider ? Icon(Icons.remove_red_eye_outlined) : Icon(Icons.image),
+            ),);
+          }),
+          Consumer(builder: (context, ref, child){
+
+            final slider = ref.watch(sliderProvider.select((state)=> state.slider));
+            print("Build 2");
+            return  Container(
+              height: 200,
+              width: 200,
+              color: Colors.red.withValues(red: slider),
             );
           }),
 
           Consumer(builder: (context, ref, child){
-            final switchProviderex = ref.watch(switchProvider);
-            print('Build--3');
-            return Switch(value: switchProviderex, onChanged: (value){
-              //ref.read(switchProviderex.notifier).re
-              ref.read(switchProvider.notifier).state=value;
-            });
+            final slider = ref.watch(sliderProvider.select((state)=> state.slider));
+            print("Build 3");
+            return     Slider(
+              value: slider,
+              onChanged: (value) {
+                final stateProvider = ref.read(sliderProvider.notifier);
+                 stateProvider.state = stateProvider.state.copyWith(slider: value);
+              },
+            );
           }),
 
-          
-          ElevatedButton(onPressed: (){
-            ref.read(counter.notifier).state++;
-          }, child: Text('+'))
+
         ],
       ),
     );
--- lib/slider_provider.dart
+++ lib/slider_provider.dart
@@ -1,8 +1,19 @@
-
-
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final sliderProvider = StateProvider<double>((ref){
-  return 0.0;
+final sliderProvider = StateProvider<AppState>((ref) {
+  return AppState(slider: 0.5, showPassword: false);
 });
+
+class AppState {
+  final double slider;
+  final bool showPassword;
+
+  AppState({required this.slider, required this.showPassword});
+
+  AppState copyWith({double? slider, bool? showPassword}) {
+    return AppState(
+      slider: slider ?? this.slider,
+      showPassword: showPassword ?? this.showPassword,
+    );
+  }
+}
